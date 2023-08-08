58c0b3974b59fa473c96c31a7a945ea858fcc991
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-08 10:56:15 -0500

    Fix item.rb archive method

--- item.rb
+++ item.rb
@@ -20,12 +20,12 @@ class Items
   end
 
   def move_to_archive
-    @archived = true if can_be_archived?
+    @archived = can_be_archived?
   end
 
   private
 
-  def can_be_archieved?
+  def can_be_archived?
     Time.new.year - publish_date > 10
   end
 end
