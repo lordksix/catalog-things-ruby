aa9d74e08ad762e0e5d040ce2408e0290da62f83
ragan <raganalemu3@gmail.com>
2023-08-08 10:51:20 +0300

    Update item class

--- item.rb
+++ item.rb
@@ -1,30 +1,22 @@
 class Items
-  attr_reader :genre, :author, :source, :label, :publish_date
+  attr_reader :genre, :author, :label, :publish_date
 
   def initialize(publish_date, archived)
     @id = Random.rand(1..1000)
-    @genre = []
-    @author = []
-    @source = []
-    @label = []
     @publish_date = publish_date
     @archived = archived
   end
 
   def add_genre(genre)
-    @genre << genre
+    @genre = genre
   end
 
   def add_author(author)
-    @author << author
-  end
-
-  def add_source(source)
-    @source << source
+    @author = author
   end
 
   def add_label(label)
-    @label << label
+    @label = label
   end
 
   def move_to_archive
