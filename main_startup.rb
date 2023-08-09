bc5e569dfc717643ee9213ff15ad02143d00ddee
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-09 04:40:45 -0500

    Implement add game

--- main_startup.rb
+++ main_startup.rb
@@ -1,3 +1,6 @@
+require_relative 'src/game'
+require_relative 'src/author'
+
 class App
   def initialize
     @books = []
@@ -32,7 +35,10 @@ class App
   end
 
   def handle_option(option)
-    if option == '11'
+    case option
+    when '10'
+      add_game
+    when '11'
       exit_app
       return
     end
@@ -87,6 +93,55 @@ class App
       @games.each do |game|
         file.puts("#{game.title}, #{game.platform}, #{game.release_year}")
       end
+    end
+  end
+
+  private
+
+  def add_game
+    puts "Input game's information"
+    author = author_details
+    game = game_details
+    new_game = Game.new(game[0], game[1], game[2])
+    new_author = Author.new(author[0], author[0])
+    new_author.add_item(new_game)
+    puts "Game by '#{author[0]}' '#{author[1]}' published in '#{game[0]}' was added successfully!"
+  end
+
+  def author_details
+    puts "Enter author's first name:"
+    first_name = gets.chomp
+    while first_name.nil?
+      puts "Enter author's first name:"
+      first_name = gets.chomp
+    end
+    puts "Enter author's last name:"
+    last_name = gets.chomp
+    while last_name.nil?
+      puts "Enter author's last name:"
+      last_name = gets.chomp
+    end
+    [first_name, last_name]
+  end
+
+  def game_details
+    publish_date = get_year("Enter game's year of publication:")
+    last_played_at = get_year('Enter last time (year) it was played:')
+    puts 'Is it Multiplayer? (Y/N)'
+    multiplayer = gets.chomp.downcase
+    while multiplayer != 'y' && multiplayer != 'n'
+      puts 'Is it Multiplayer? (Y/N)'
+      multiplayer = gets.chomp.downcase
+    end
+    [publish_date, multiplayer == 'y', last_played_at]
+  end
+
+  def get_year(msg)
+    puts msg
+    year = gets.chomp.to_i
+    while year < 1000 || year > 2500
+      puts msg
+      year = gets.chomp.to_i
     end
   end
 end
