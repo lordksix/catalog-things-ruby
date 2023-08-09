b4b7cda9ac084b81280802d070c58146cfcc6a37
lordksix <121689749+lordksix@users.noreply.github.com>
2023-08-09 03:39:08 -0500

    Refactor private methods

--- src/game.rb
+++ src/game.rb
@@ -1,7 +1,7 @@
 require_relative '../item'
 
 class Game < Items
-  def initialize(publish_date, archived, multiplayer, last_played_at)
+  def initialize(publish_date, multiplayer, last_played_at, archived: false)
     super(publish_date, archived)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
@@ -10,6 +10,6 @@ class Game < Items
   private
 
   def can_be_archived?
-    Time.new.year - publish_date > 10 && Time.new.year - @last_played_at > 2
+    super && Time.new.year - @last_played_at > 2
   end
 end
